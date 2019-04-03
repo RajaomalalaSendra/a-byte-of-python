ff541fb8c264f9b4fe6e52a696725579887bdb68
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-03 07:19:58 +0300

    this is the last last modification in the ...

--- the_with_exception.py
+++ the_with_exception.py
@@ -1,5 +1,8 @@
 import time
-with open("poem.txt") as f:
-    for line in f:
-        print(line,)
-        time.sleep(2)
+try:
+    with open("peee.txt") as f:
+        for line in f:
+            print(line,)
+            time.sleep(2)
+except IOError:
+	print("That file doesn't exist.")
