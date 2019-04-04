f99bcda4a3dfdd0905a08eaadecf7f43db117a6c
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-04 07:03:08 +0300

    creation of the match re in this repo

--- regular_expression/match_re.py
+++ regular_expression/match_re.py
@@ -6,14 +6,21 @@ regexes = [
     re.compile(p)
     for p in word.split(" ")
 ]
-text = 'Does this text match the pattern?'
+lister = list()
+
+try:
+	with open('text.txt') as text:
+		for t in text:
+			lister.append(t)
+except IOError:
+	print("Cannot import that text.")
+text = " ".join(lister)
 
 print('Text: {!r}\n'.format(text))
 
 for regex in regexes:
     print('Seeking "{}" ->'.format(regex.pattern),
           end=' ')
-
     if regex.search(text):
         print('match!')
     else:
